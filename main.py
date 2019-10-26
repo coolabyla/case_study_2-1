55b875b94f8f6d873d4ad24e7ec4bbe73af28998
beunick13 <beushevnv@gmail.com>
2019-10-26 16:15:20 +0700

    Working cicle

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-# Case 2
-# Developers:
-#
-#
+# Case 1
+# Developers:   Nikita Beushev (70%)
+#               Alexandra Kozadeeva (60%)
+#               Anzhelika Kurepova (55%)
+
+# TODO:
+
+
+import calendar
+JAN = 'янв'
+FEB = 'фев'
+MAR = 'мар'
+APR = 'апр'
+MAY = 'май'
+JUN = 'июнь'
+JUL = 'июль'
+AUG = 'авг'
+SEP = 'сен'
+OCT = 'окт'
+NOV = 'нов'
+DEC = 'дек'
+QUESTION = 'Сколько денег вы заработали в {}: '
 
 # string constants
-name_month = [JAN, FAB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC]
+name_month = [JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC]
 
 annual_income = 0
 for month in range(12):
-    print('{} {}:'.format(QUESTION,name_month[month], end ='')
-    income = float(input())
+    income = int(input(QUESTION.format(name_month[month])))
     annual_income += income
 print(annual_income)
