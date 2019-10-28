1a0bc4432f168c47d3578c2269bea18a464aa25a
beunick13 <beushevnv@gmail.com>
2019-10-29 01:17:23 +0700

    Working cicle

--- main.py
+++ main.py
@@ -7,25 +7,27 @@
 
 
 import calendar
-JAN = 'янв'
-FEB = 'фев'
-MAR = 'мар'
-APR = 'апр'
-MAY = 'май'
-JUN = 'июнь'
-JUL = 'июль'
-AUG = 'авг'
-SEP = 'сен'
-OCT = 'окт'
-NOV = 'нов'
-DEC = 'дек'
-QUESTION = 'Сколько денег вы заработали в {}: '
+JAN = 'январе'
+FEB = 'феврале'
+MAR = 'марте'
+APR = 'апреле'
+MAY = 'мае'
+JUN = 'июне'
+JUL = 'июле'
+AUG = 'августе'
+SEP = 'сентябре'
+OCT = 'октябре'
+NOV = 'ноябре'
+DEC = 'декабре'
+QUESTION1 = 'К какой категории вас можно отнести? '
+QUESTION2 = 'Сколько денег вы заработали в {}: '
+QUESTION3 = 'Какой у вас необлагаемый налогом доход?'
 
 # string constants
 name_month = [JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC]
 
 annual_income = 0
 for month in range(12):
-    income = int(input(QUESTION.format(name_month[month])))
+    income = int(input(QUESTION2.format(name_month[month])))
     annual_income += income
 print(annual_income)
