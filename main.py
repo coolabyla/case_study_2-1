752a183003831d26311cd521601c4e4eafb16b5a
beunick13 <beushevnv@gmail.com>
2019-10-30 22:03:13 +0700

    Questions and localization

--- main.py
+++ main.py
@@ -18,22 +18,22 @@ for month in range(12):
 print(annual_income)
 
 # TODO: (Anzhelika) one person
-if category == 'один субъект' and annual_income >= 0 and annual_income <= 9075:
+if category == 1 and annual_income >= 0 and annual_income <= 9075:
     print(0.1 * (annual_income - deduction))
-elif category == 'один субъект' and annual_income >= 9076 and annual_income <= 36_900:
+elif category == 1 and annual_income >= 9076 and annual_income <= 36_900:
     print(0.15 * (annual_income - 9076 - deduction) + 0.1 * (9076 - 0))
-elif category == 'один субъект' and annual_income >= 36_901 and annual_income <= 89_350:
+elif category == 1 and annual_income >= 36_901 and annual_income <= 89_350:
     print(0.25 * (annual_income - 36_901 - deduction) + 0.1 * (9_076 - 0) + 0.15 * (36_901 - 9076))
-elif category == 'один субъект' and annual_income >= 89_351 and annual_income <= 186_350:
+elif category == 1 and annual_income >= 89_351 and annual_income <= 186_350:
     print(0.28 * (annual_income - 89_351 - deduction) + 0.1 * (9076 - 0) + 0.15 * (36_901 - 9076) +
           + 0.25 * (89_351 - 36_901))
-elif category == 'один субъект' and annual_income >= 186_351 and annual_income <= 405_100:
+elif category == 1 and annual_income >= 186_351 and annual_income <= 405_100:
     print(0.33 * (annual_income - 186_351 - deduction) + 0.1 * (9076 - 0) + 0.15 * (36_901 - 9076) +
           + 0.25 * (89_351 - 36_901) + 0.28 * (186_351 - 89_351))
-elif category == 'один субъект' and annual_income >= 405_101 and annual_income <= 406_750:
+elif category == 1 and annual_income >= 405_101 and annual_income <= 406_750:
     print(0.35 * (annual_income - 405_101 - deduction) + 0.1 * (9076 - 0) + 0.15 * (36_901 - 9076) +
          + 0.25 * (89_351 - 36_901) + 0.28 * (186_351 - 89_351) + 0.33 * (405_101 - 186_351))
-elif category == 'один субъект' and annual_income >= 406_751:
+elif category == 1 and annual_income >= 406_751:
     print(0.396 * (annual_income - 406_751 - deduction) + 0.1 * (9076 - 0) + 0.15 * (36_901 - 9076) +
           + 0.25 * (89_351 - 36_901) + 0.28 * (186_351 - 89_351) + 0.33 * (405_101 - 186_351) +
           + 0.35 * (406_751 - 405_101))
@@ -61,21 +61,21 @@ elif category == 'один субъект' and annual_income >= 406_751:
 
 
 # TODO: (Nikita) single parent
-if category == 'один субъект' and annual_income >= 0 and annual_income <= 12950:
+if category == 3 and 0 <= annual_income <= 12950:
     print(0.1 * (annual_income - deduction))
-elif category == 'один субъект' and annual_income >= 12_951 and annual_income <= 49_400:
+elif category == 3 and 12_951 <= annual_income <= 49_400:
     print(0.15 * (annual_income - 12_951 - deduction) + 0.1 * (12951 - 0))
-elif category == 'один субъект' and annual_income >= 49_401 and annual_income <= 127_550:
+elif category == 3 and 49_401 <= annual_income <= 127_550:
     print(0.25 * (annual_income - 49_401 - deduction) + 0.1 * (9_076 - 0) + 0.15 * (49_401 - 12_951))
-elif category == 'один субъект' and annual_income >= 127_551 and annual_income <= 206_600:
+elif category == 3 and 127_551 <= annual_income <= 206_600:
     print(0.28 * (annual_income - 127_551 - deduction) + 0.1 * (12_951 - 0) + 0.15 * (49_401 - 12_951) +
           + 0.25 * (127_551 - 49_401))
-elif category == 'один субъект' and annual_income >= 206_601 and annual_income <= 405_100:
+elif category == 3 and 206_601 <= annual_income <= 405_100:
     print(0.33 * (annual_income - 206_601 - deduction) + 0.1 * (12951 - 0) + 0.15 * (49_401 - 12_951) +
           + 0.25 * (127_551 - 49_401) + 0.28 * (206_601 - 127_551))
-elif category == 'один субъект' and annual_income >= 405_101 and annual_income <= 432_200:
+elif category == 3 and 405_101 <= annual_income <= 432_200:
     print(0.35 * (annual_income - 405_101 - deduction) + 0.1 * (12951 - 0) + 0.15 * (49_401 - 12_951) +
-         + 0.25 * (127_551 - 49_401) + 0.28 * (206_601 - 127_551) + 0.33 * (405_101 - 206_601))
+          + 0.25 * (127_551 - 49_401) + 0.28 * (206_601 - 127_551) + 0.33 * (405_101 - 206_601))
 else:
     print(0.396 * (annual_income - 432_201 - deduction) + 0.1 * (12951 - 0) + 0.15 * (49_401 - 12951) +
           + 0.25 * (127_551 - 49_401) + 0.28 * (206_601 - 127_551) + 0.33 * (405_101 - 206_601 +
