94c92a274bb53047c4b32b19d4a222e9abfb674e
coolabyla <56190502+coolabyla@users.noreply.github.com>
2019-10-31 17:14:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,15 @@
 #               Alexandra Kozadeeva ()
 #               Anzhelika Kurepova ()
 
-# localization
-import local as lc
+import pandas as pd
+import localization as lc
+
+# input data
+tax_category_data = pd.read_csv('tax_category_data.csv')
+name_month = lc.month_dict
+
 category = int(input(lc.QUESTION1))
 deduction = int(input(lc.QUESTION2))
-
-# string constants
-name_month = [lc.JAN, lc.FEB, lc.MAR, lc.APR, lc.MAY, lc.JUN, lc.JUL, lc.AUG, lc.SEP, lc.OCT, lc.NOV, lc.DEC]
 
 annual_income = 0
 for month in range(12):
@@ -18,69 +20,44 @@ for month in range(12):
 
 taxable_income = int(annual_income - deduction)
 
-# TODO: (Anzhelika) one person
-if category == 1 and annual_income >= 0 and annual_income <= 9075:
-    tax = (0.1 * taxable_income)
-elif category == 1 and annual_income >= 9076 and annual_income <= 36_900:
-    tax = (0.15 * (taxable_income - 9076) + 0.1 * (9076 - 0))
-elif category == 1 and annual_income >= 36_901 and annual_income <= 89_350:
-    tax = (0.25 * (taxable_income - 36_901) + 0.1 * (9_076 - 0) + 0.15 * (36_901 - 9076))
-elif category == 1 and annual_income >= 89_351 and annual_income <= 186_350:
-    tax = (0.28 * (taxable_income - 89_351) + 0.1 * (9076 - 0) + 0.15 * (36_901 - 9076) +
-          + 0.25 * (89_351 - 36_901))
-elif category == 1 and annual_income >= 186_351 and annual_income <= 405_100:
-    tax = (0.33 * (taxable_income - 186_351) + 0.1 * (9076 - 0) + 0.15 * (36_901 - 9076) +
-          + 0.25 * (89_351 - 36_901) + 0.28 * (186_351 - 89_351))
-elif category == 1 and annual_income >= 405_101 and annual_income <= 406_750:
-    tax = (0.35 * (taxable_income - 405_101) + 0.1 * (9076 - 0) + 0.15 * (36_901 - 9076) +
-          + 0.25 * (89_351 - 36_901) + 0.28 * (186_351 - 89_351) + 0.33 * (405_101 - 186_351))
-elif category == 1 and annual_income >= 406_751:
-    tax = (0.396 * (taxable_income - 406_751) + 0.1 * (9076 - 0) + 0.15 * (36_901 - 9076) +
-          + 0.25 * (89_351 - 36_901) + 0.28 * (186_351 - 89_351) + 0.33 * (405_101 - 186_351) +
-          + 0.35 * (406_751 - 405_101))
 
-# TODO: (Alexandra) married couple
+# solution
+def tax_calculation(taxable_income_int=taxable_income,
+                    category_int=category):
+# расчет накопленных сумм
+    tax_1 = (int(tax_category_data['Income_' + str(category_int) + '_category'][0]) + 1) * float(tax_category_data['%'][0])
+    tax_2 = tax_1 + (int(tax_category_data['Income_' + str(category_int) + '_category'][1]) -
+                     int(tax_category_data['Income_' + str(category_int) + '_category'][0])) * float(tax_category_data['%'][1])
+    tax_3 = tax_2 + (int(tax_category_data['Income_' + str(category_int) + '_category'][2]) -
+                     int(tax_category_data['Income_' + str(category_int) + '_category'][1])) * float(tax_category_data['%'][2])
+    tax_4 = tax_3 + (int(tax_category_data['Income_' + str(category_int) + '_category'][3]) -
+                     int(tax_category_data['Income_' + str(category_int) + '_category'][2])) * float(tax_category_data['%'][3])
+    tax_5 = tax_4 + (int(tax_category_data['Income_' + str(category_int) + '_category'][4]) -
+                     int(tax_category_data['Income_' + str(category_int) + '_category'][3])) * float(tax_category_data['%'][4])
+    tax_6 = tax_5 + (int(tax_category_data['Income_' + str(category_int) + '_category'][5]) -
+                     int(tax_category_data['Income_' + str(category_int) + '_category'][4])) * float(tax_category_data['%'][5])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if 0 <= annual_income <= int(tax_category_data['Income_' + str(category_int) + '_category'][0]):
+        tax = taxable_income_int * float(tax_category_data['%'][0])
+    elif annual_income <= int(tax_category_data['Income_' + str(category_int) + '_category'][1]):
+        tax = tax_1 + (taxable_income_int - (int(tax_category_data['Income_' + str(category_int) + '_category'][0]) + 1)) \
+                        * float(tax_category_data['%'][0])
+    elif annual_income <= int(tax_category_data['Income_' + str(category_int) + '_category'][2]):
+        tax = tax_2 + (taxable_income_int - (int(tax_category_data['Income_' + str(category_int) + '_category'][1]) + 1)) \
+                        * float(tax_category_data['%'][1])
+    elif annual_income <= int(tax_category_data['Income_' + str(category_int) + '_category'][3]):
+        tax = tax_3 + (taxable_income_int - (int(tax_category_data['Income_' + str(category_int) + '_category'][2]) + 1)) \
+                        * float(tax_category_data['%'][2])
+    elif annual_income <= int(tax_category_data['Income_' + str(category_int) + '_category'][4]):
+        tax = tax_4 + (taxable_income_int - (int(tax_category_data['Income_' + str(category_int) + '_category'][3]) + 1)) \
+                        * float(tax_category_data['%'][3])
+    elif annual_income <= int(tax_category_data['Income_' + str(category_int) + '_category'][5]):
+        tax = tax_5 + (taxable_income_int - (int(tax_category_data['Income_' + str(category_int) + '_category'][4]) + 1)) \
+                        * float(tax_category_data['%'][4])
+    else:
+        tax = tax_6 + (taxable_income_int - (int(tax_category_data['Income_' + str(category_int) + '_category'][5]) + 1)) \
+                        * float(tax_category_data['%'][5])
+    return tax
 
 
-
-# TODO: (Nikita) single parent
-if category == 3 and 0 <= annual_income <= 12950:
-    tax = (0.1 * taxable_income)
-elif category == 3 and 12_951 <= annual_income <= 49_400:
-    tax = (0.15 * (taxable_income - 12_951) + 0.1 * (12951 - 0))
-elif category == 3 and 49_401 <= annual_income <= 127_550:
-    tax = (0.25 * (taxable_income - 49_401) + 0.1 * (9_076 - 0) + 0.15 * (49_401 - 12_951))
-elif category == 3 and 127_551 <= annual_income <= 206_600:
-    tax = (0.28 * (taxable_income - 127_551) + 0.1 * (12_951 - 0) + 0.15 * (49_401 - 12_951) +
-           + 0.25 * (127_551 - 49_401))
-elif category == 3 and 206_601 <= annual_income <= 405_100:
-    tax = (0.33 * (taxable_income - 206_601) + 0.1 * (12951 - 0) + 0.15 * (49_401 - 12_951) +
-           + 0.25 * (127_551 - 49_401) + 0.28 * (206_601 - 127_551))
-elif category == 3 and 405_101 <= annual_income <= 432_200:
-    tax = (0.35 * (taxable_income - 405_101) + 0.1 * (12951 - 0) + 0.15 * (49_401 - 12_951) +
-           + 0.25 * (127_551 - 49_401) + 0.28 * (206_601 - 127_551) + 0.33 * (405_101 - 206_601))
-else:
-    tax = (0.396 * (taxable_income - 432_201) + 0.1 * (12951 - 0) + 0.15 * (49_401 - 12951) +
-           + 0.25 * (127_551 - 49_401) + 0.28 * (206_601 - 127_551) + 0.33 * (405_101 - 206_601 +
-           + 0.35 * (432_201 - 405_101)))
-
-print(lc.TAX, format(tax, '.2f'))
-
+print(lc.TAX, format(tax_calculation(), '.2f'))
